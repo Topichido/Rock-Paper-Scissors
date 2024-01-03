97615b931ff14f41d4c9ef1454e9d1831ccbd2c2
Topichido <90105233+Topichido@users.noreply.github.com>
2024-01-03 23:03:57 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,20 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const RockPaperScissors = await ethers.getContractFactory("RockPaperScissors");
+  const rockPaperScissors = await RockPaperScissors.deploy();
+
+  console.log("RockPaperScissors deployed to:", rockPaperScissors.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
